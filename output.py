3c7ede55e69eeb90e455264e7f261891d6abeec1
codedecde <barunpatra95@gmail.com>
2015-09-28 15:48:28 +0530

    Something

--- output.py
+++ output.py
@@ -1,53 +1,63 @@
 fin = open('problem.plan')
-fout = open('output.txt', 'w')
-
-number_of_steps = 0
-for line in fin:
-	if (bool('Plan length: ' in line)):
-		line = line.split()
-		number_of_steps = eval(line[2])
-fin.close()
-
-fin = open('problem.plan')
+#fout = open('output.txt', 'w')
 
 counter = 0
 all_moves = []
 moves = []
-current_car = prev_car = '' 
+buffer  = []
+
 for line in fin:
 	if (bool('move_' in line)):
-		#if ( counter == 0):
-		current_car = line.split()[1]
-		if(current_car == prev_car):
-			moves.append(line)
+		buffer.append(line)
+
+visited = set()
+car_data = {}
+#car_data = { 1:[lenght,start_pos,flag] }
+col = -1
+for line in buffer:
+	line = line.split()
+	if (not line[1] in visited):
+		visited.add(line[1])
+		list_Item = [0,0,'']
+		if(line[0] == 'move_left' or line[0] == 'move_right'):
+			list_Item[2] = 'H'
+			list_Item[0] = abs(eval(line[2].lstrip('sq-')) - eval(line[3].lstrip('sq-'))) + 1
+			list_Item[1] = eval(line[2].lstrip('sq-'))
+			car_data[line[1]] = []
+			for item in list_Item:
+				car_data[line[1]].append(item)
 		else:
-			prev_car = current_car
-			if(len(moves) != 0):
-				all_moves.append(moves)
-			moves = []
-			moves.append(line)
+			col = abs(eval(line[2].lstrip('sq-')) - eval(line[4].lstrip('sq-')))
+			list_Item[2] = 'V'
+			list_Item[0] = (abs(eval(line[2].lstrip('sq-')) - eval(line[3].lstrip('sq-'))) / col ) + 1
+			list_Item[1] = eval(line[2].lstrip('sq-'))
+			car_data[line[1]] = []
+			for item in list_Item:
+				car_data[line[1]].append(item)
+
+for item in car_data.keys():
+	print item, car_data[item]
+
+current_car = prev_car = '' 
+
+for line in buffer:
+	current_car = line.split()[1]
+	if(current_car == prev_car):
+		moves.append(line)
+	else:
+		prev_car = current_car
+		if(len(moves) != 0):
+			all_moves.append(moves)
+		moves = []
+		moves.append(line)
 all_moves.append(moves)
+
 fout.write(str(len(all_moves)) + '\n')
+
 for lines in all_moves:
 	line = lines[0]
 	line = line.split('-')
 	fout.write(line[1][0] + ' ')
 	line = line[0].split('_')	
 	fout.write(str(line[1][0]).upper() + ' ' + str(len(lines)) + '\n')
-			#counter = counter + 1
-'''else:
-			line1 = line.split('-')
-			if ( line1[0] == templine[0] and line1[1][0] == templine[1][0] ):
-				counter = counter + 1
-				print('helo\n')
-			else:
-				printline = templine
-				fout.write(printline[1][0] + ' ')
-				printline = printline[0].split('_')
-				fout.write(str(printline[1][0]).upper() + ' ' + str(counter) + '\n')
-				templine = line.split('-')
-				counter = 1
-
-fout.write(templine[1][0] + ' ')
-templine = templine[0].split('_')
-fout.write(str(templine[1][0]).upper() + ' ' + str(counter) + '\n')'''
+			
